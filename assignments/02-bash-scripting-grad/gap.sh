4f50b6e6e1edd3d58406a866e715d93b3675e170
cmbrown89 <clairessabrown@email.arizona.edu>
2019-01-30 10:08:35 -0700

    almost finished

--- assignments/02-bash-scripting-grad/gap.sh
+++ assignments/02-bash-scripting-grad/gap.sh
@@ -1,29 +1,57 @@
 #!/usr/bin/env bash
 
-set -u
+#set -u
 
 # If there are no arguments, print out all the basenames of the files 
 # in sorted order
 
 RELPATH="../../data/gapminder/"
+FILES=$(mktemp)
 
-# This works
+#mv $RELPATHall.txt $RELPATHall.cc.txt
 
+i=0
 if [[ $# -eq 0 ]]; then
-    cd $RELPATH 
-    ls | sort
-    exit
+	find $RELPATH -type f -iname "*" | sort > "$FILES"
+ 	while read -r LINE; do
+		i=$((i+1))
+		BASENAME=$(basename $LINE .cc.txt)
+ 	    printf "    $i $BASENAME\n"
+		done < "$FILES"
 fi
 
 # If there is an argument, treat it like a regular expression and 
 # find files where the basename matches at the beginning of the string
 # in a case-insensitive manner and print them in sorted order
 
+# If no files are found, print a message telling the user
+
 REGEX=$1
 
-if [[ $# -eq 1 ]]; then
-	cd $RELPATH
-	LIST=$(ls | sort)
-	find -type f -iname "$REGEX*" | basename | sort 
+find $RELPATH -type f -iname "$REGEX*" | sort > $FILES
+NUM_FILES=$(wc -l "$FILES" | awk '{print $1}')
+
+if [[ $NUM_FILES -lt 1 ]]; then
+	printf "There are no countries starting with \"%s\"\n" $REGEX
 fi
+
+
+if [[ $NUM_FILES -gt 1 ]]; then
+	while read -r LINE; do
+	i=$((i+1))
+	BASENAME=$(basename "$LINE" .cc.txt)
+    printf "%4d  %s\n" $i $BASENAME
+	done < "$FILES"
+fi
+
+
+# loop to get line numbers from error message in test.py
+#i=0
+#while read -r LINE; do
+#	i=$((i+1))
+#	echo "$i $LINE"
+#done < test.py
+
+
+
 
