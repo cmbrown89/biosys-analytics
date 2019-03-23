0ec6d78a511dea47f7cf372d357e8b329d3f3ed4
cmbrown89 <clairessabrown@email.arizona.edu>
2019-03-23 16:18:30 -0700

    finally figured this out

--- assignments/09-grad-swissprot/swisstake.py
+++ assignments/09-grad-swissprot/swisstake.py
@@ -9,6 +9,7 @@ import argparse
 import sys
 from Bio import SeqIO
 import re
+import os
 
 
 # --------------------------------------------------
@@ -19,29 +20,31 @@ def get_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument(
-        'file', metavar='str', help='A Swissprot file')
-
-    parser.add_argument(
-        '-k',
-        '--keyword',
-        help='Keyword to search for',
-        metavar='keyword to match',
-        type=str,
-        require=True)
+        'file', metavar='FILE', help='A Swissprot file')
 
     parser.add_argument(
         '-s',
         '--skip',
         help='Skip records by given taxa',
-        metavar='str',
+        metavar='STR',
         type=str,
-        nargs="+")
+        nargs="+",
+        default=None)
+   
+    parser.add_argument(
+        '-k',
+        '--keyword',
+        help='Keyword to search for',
+        metavar='STR',
+        type=str,
+        required=True,
+        default=None)
 
     parser.add_argument(
         '-o',
         '--outfile',
         help='output file',
-        metavar='str',
+        metavar='FILE',
         type=str,
         default='out.fa')
 
@@ -71,73 +74,40 @@ def main():
     out = args.outfile
 
 
-    # Read in records to dictionary (annotations)
-    # Search for keyword
+    if not os.path.isfile(swiss):
+        die("\"{}\" is not a file".format(swiss)) 
 
-    print("Processing \"swiss.txt\"")
+    if skip is not None:
+        set_skip = set([e.upper() for e in skip])
 
 
-
-    """
-    Goal: search all keywords for keywords, skip certain keywords or taxa
-    - make user input sets for easy comparisons
-    - skip to decrease computation time
-    - 
-
-    """
-
-    set_skip = set([e.upper() for e in skip])
-    set_keyword = set([keyword.upper()])
+    if len(keyword) > 1:
+        set_keyword = set([keyword.upper()])
 
     num_skipped = 0
     num_taken = 0
-    
-
-    with open(outfile, "w") as outfh:
-    for record in SeqIO.parse(swiss, "swiss"):
-        annotations = record.annotations
-        if set_skip and "taxonomy"  in annotations:
-            taxa = set(map(str.lower, annotations["taxonomy"]))
-            if set_skip.intersection(taxa):
-                num_skipped +=1
-                continue
-            if "keywords" in annotations:
-                kw = set(map(str.lower, annotations["keywords"]))
-
-                if k in kw: # or make a set operation since both are
-                    num_taken+=1
-                    SeqIO.write(record, outfh, "fasta")
-                else:
-                    num_skipped+= 1
-
-    print("Done, skipped {} and took {}. See output in \"{}\"".format(num_skipped, num_taken, outfh))
-    
 
 
+    print("Processing \"swiss.txt\"")
 
+    with open(out, "w") as outfh:
+        for record in SeqIO.parse(swiss, "swiss"):
+            annotations = record.annotations # call r.a dict annotations
+            if skip and "taxonomy"  in annotations: # if the words to skip AND the category "taxonomy" is in the record dictionary, check for certain conditions below...
+                # iterate through each record in the record dict (annotations) and make taxonomy list into a set
+                taxa = set(map(str.upper, annotations["taxonomy"])) 
+                if set_skip.intersection(taxa): # if the skip words intesect with any of the taxonomy...
+                    num_skipped +=1 # count this as part of records skipped
+                    continue # and then move on and continue the loop
+            if "keywords" in annotations: # check if category keywords in record dict,...
+                kw = set(map(str.upper, annotations["keywords"])) # if it is make keywords in record dict into a set
+                if kw.intersection(set_keyword): # if keywords from user intersect with keywords from record dict
+                    num_taken += 1 # count as taken
+                    SeqIO.write(record, outfh, "fasta") # and write the record that matches this keyword to the ooutfile
+                else:                      
+                    num_skipped +=1 # else, count as skipped
 
-
-def blah():
-
-    """main"""
-    args = get_args()
-    file = args.file
-    
-    for i, record in enumerate(SeqIO.parse(file, "swiss"), start=1):
-        # printing number and record id
-        print('{:3}: {}'.format(i, record.id))
-        annotations = record.annotations
-        
-        # below record id...
-        for annot_type in ['accessions', 'keywords', 'taxonomy']:
-            if annot_type in annotations: # search annotations dict for above list
-                print('\tANNOT {}:'.format(annot_type)) 
-                val = annotations[annot_type] # search for elements of above list in annotations
-                if type(val) is list:
-                    for v in val:
-                        print('\t\t{}'.format(v))
-                else:
-                    print('\t\t{}'.format(val))
+    print("Done, skipped {} and took {}. See output in \"{}\".".format(num_skipped, num_taken, out))
 
 
 # --------------------------------------------------
@@ -145,7 +115,3 @@ if __name__ == '__main__':
     main()
 
 
-swiss = "swiss.txt"
-for record in SeqIO.parse(swiss, "swiss"):
-    annotations = record.annotations
-    print(annotations)
